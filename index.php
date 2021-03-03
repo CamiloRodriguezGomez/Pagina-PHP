93fd52c0df40bf3fd2d5567238373f38d278d34a
CamiloRodriguezGomez <camilorexpro2006@gmail.com>
2021-03-03 16:48:02 -0500

    Primer Commit
    Copie el contenido de los archivos dentro del index

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <!DOCTYPE html>
 <html lang="es">
-<?php require_once 'head.php'; ?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Amilcar Gómez">
+    <meta name="description" content="Pagina Demo de Estudio con PHP">
+</head>
 <title>Pagina Compuestudio</title>
 <body>
-    <?php require_once 'main.php'; ?>
+<main>
+        <div>
+            <img src="recursos/imagenes/logo-banner-php.jpg" width="100%" alt="">
+            <cite>Referencia web <a href="https://servreality.com/wp-content/uploads/2020/07/PHP-Development-Banner.jpg" target="_blank" >IR</a></cite>
+        </div>
+        <div>
+            <nav>
+                <a href="index.php">Inicio</a>
+                <a href="imagenes.php">Imagenes de PHP</a>
+            </nav>
+        </div>
+</main>
     <header>
         <div align="center">
             <h1>¿Que es PHP?</h1>
@@ -13,6 +30,13 @@
             <cite>Referencia web <a href="https://es.wikipedia.org/wiki/PHP" target="_blank" >Wikipedia</a></cite>
         </div><br>
     </header>
-    <?php require_once 'footer.php'; ?>
+    <footer>
+        <div>
+            <address>
+                Autor: Amilcar Gómez
+                Corrreo <a href="mailto:amilcar@compuestudio.edu.co">amilcar@compuestudio.edu.co</a>
+            </address>
+        </div>
+</footer>
 </body>
 </html>
