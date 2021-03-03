76f0cbd6337c053519942634af6bb60513054238
Camilo_Rodriguez_G <camilorexpro2006@gmail.com>
2021-03-03 15:51:25 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Amilcar Gómez">
+    <meta name="description" content="Pagina Demo de Estudio con PHP">
+    <title>Página PHP</title>
+</head>
+<body>
+    <main>
+        <div>
+            <img src="recursos/imagenes/logo-banner-php.jpg" width="100%" alt="">
+            <cite>Referencia web <a href="https://servreality.com/wp-content/uploads/2020/07/PHP-Development-Banner.jpg" target="_blank" >IR</a></cite>
+        </div>
+        <div>
+            <nav>
+                <a href="index.php">Inicio</a>
+                <a href="imagenes.php">Imagenes de PHP</a>
+            </nav>
+        </div>
+    </main>
+    <header>
+        <div align="center">
+            <h1>¿Que es PHP?</h1>
+        </div>
+        <div>
+            <p>PHP es un lenguaje de programación de uso general que se adapta especialmente al desarrollo web.2​ Fue creado inicialmente por el programador danés-canadiense Rasmus Lerdorf en 1994.3​ En la actualidad, la implementación de referencia de PHP es producida por The PHP Group.4​ PHP originalmente significaba Personal Home Page (Página personal), pero ahora significa el inicialismon 1​ recursivo PHP: Hypertext Preprocessor.</p>
+            <cite>Referencia web <a href="https://es.wikipedia.org/
+    
